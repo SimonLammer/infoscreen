1d205bacfbeae5a3de07f9b1b2199793b42b6ebe
Simon Lammer <lammer.simon@gmail.com>
2016-06-09 21:26:39 +0200

    Add views list

---
+++ javascript/entities.js
@@ -0,0 +1,29 @@
+/*
+orientation may be any of:
++ top
++ right
++ bottom
++ left
+
+distance may be any css distance (e.g. 70px)
+
+new Align('top', '70px); // 70px from the top border
+*/
+function Align(orientation, distance) {
+	this.orientation = orientation;
+	this.distance = distance;
+}
+
+// View
+function View() {
+	this.id = View._lastid === undefined
+		? (View._lastid = 0)
+		: ++View._lastid;
+	this.name = "View" + this.id;
+	this.verticalAlign = new Align('top', '0px');
+	this.horizontalAlign = new Align('left', '0px');
+	this.height = '100px';
+	this.width = '100px';
+	this.zindex = 0;
+	this.rotation = 0;
+}
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -2,11 +2,16 @@ window.previewSize = {
 	'width': '800px',
 	'height': '300px'
 }
+window.views = [];
+views.push(new View());
+views.push(new View());
+views.push(new View());
 
 $(document).ready(function() {
 	sightglass.adapters = rivets.adapters;
 	sightglass.root = '.';
 	initPreviewcontrol();
+	initViewcontrol();
 });
 
 function initPreviewcontrol() {
@@ -17,4 +22,22 @@ function initPreviewcontrol() {
 	sightglass(previewSize, 'width', function() {
 		$('#preview').css('width', previewSize.width);
 	});
+}
+
+function initViewcontrol() {
+	rivets.bind($('#viewcontrol'), {'viewslist': views});
+	$('#button-add_view').click(function() {
+		views.push(new View());
+	});
+	/* */
+	sightglass({data: views}, 'data', function() {
+		$('#viewslist button.new').each(function(e) {
+			var viewid = $(this).parent().find('.viewid').val();
+			$(this).click(function() {
+				alert(viewid);
+			})
+			.removeClass('new');
+		});
+	});
+	/* */
 }
