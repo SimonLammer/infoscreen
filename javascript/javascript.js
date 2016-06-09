7f8605ce763ceabb5ec0e0bc26ac18363b568c87
Simon Lammer <lammer.simon@gmail.com>
2016-06-09 20:16:59 +0200

    Use Rivets.js for data binding

--- javascript/javascript.js
+++ javascript/javascript.js
@@ -1,15 +1,20 @@
+window.previewSize = {
+	'width': '800px',
+	'height': '300px'
+}
+
 $(document).ready(function() {
-	initPreviewControl();
+	sightglass.adapters = rivets.adapters;
+	sightglass.root = '.';
+	initPreviewcontrol();
 });
 
-function initPreviewControl() {
-	$('#previewcontrol').submit(function(e) {
-		e.preventDefault();
-		var width = $('#previewwidth').val();
-		var height = $('#previewheight').val();
-		$('#preview').css({
-			'width': width,
-			'height': height
-		});
+function initPreviewcontrol() {
+	window.previewcontrolbind = rivets.bind($('#previewcontrol'), window.previewSize);
+	sightglass(previewSize, 'height', function() {
+		$('#preview').css('height', previewSize.height);
+	});
+	sightglass(previewSize, 'width', function() {
+		$('#preview').css('width', previewSize.width);
 	});
 }
