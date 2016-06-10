952b5e5179e7044ff5ba359f61eba2b0aa9486b0
Simon Lammer <lammer.simon@gmail.com>
2016-06-10 11:37:44 +0200

    update edit mode

--- javascript/entities.js
+++ javascript/entities.js
@@ -9,6 +9,10 @@ distance may be any css distance (e.g. 70px)
 
 new Align('top', '70px); // 70px from the top border
 */
+window.alignOrientations = {
+	vertical: ['top', 'bottom'],
+	horizontal: ['right', 'left']
+};
 function Align(orientation, distance) {
 	this.orientation = orientation;
 	this.distance = distance;
@@ -25,5 +29,4 @@ function View() {
 	this.height = '100px';
 	this.width = '100px';
 	this.zindex = 0;
-	this.rotation = 0;
 }
--- javascript/javascript.js
+++ javascript/javascript.js
@@ -3,16 +3,21 @@ window.previewSize = {
 	'height': '300px'
 }
 window.views = [];
-views.push(new View());
-views.push(new View());
-views.push(new View());
 
 $(document).ready(function() {
 	sightglass.adapters = rivets.adapters;
 	sightglass.root = '.';
+	initPreview();
 	initPreviewcontrol();
 	initViewcontrol();
+
+	// debug
+	views.push(new View());
 });
+
+function initPreview() {
+	window.previewview = rivets.bind($('#previewviews'), {'viewslist': views});
+}
 
 function initPreviewcontrol() {
 	window.previewcontrolbind = rivets.bind($('#previewcontrol'), window.previewSize);
@@ -25,19 +30,47 @@ function initPreviewcontrol() {
 }
 
 function initViewcontrol() {
-	rivets.bind($('#viewcontrol'), {'viewslist': views});
+	rivets.bind($('#viewcontrol'), {
+		'viewslist': views,
+		'verticalalignorientations': alignOrientations.vertical,
+		'horizontalalignorientations': alignOrientations.horizontal,
+		'updatePreview': refreshPreview
+	});
 	$('#button-add_view').click(function() {
 		views.push(new View());
 	});
-	/* */
 	sightglass({data: views}, 'data', function() {
 		$('#viewslist button.new').each(function(e) {
-			var viewid = $(this).parent().find('.viewid').val();
 			$(this).click(function() {
-				alert(viewid);
+				var viewid = $(this).parent().find('.viewid').val();
+				for (var i = 0; i < views.length; i++) {
+					if (views[i].id == viewid) {
+						views.splice(i, 1);
+						break;
+					}
+				}
 			})
 			.removeClass('new');
 		});
+		refreshPreview();
 	});
-	/* */
+}
+
+function refreshPreview() {
+		for (var i = 0; i < views.length; i++) {
+			$('#previewviews div[viewid="' + views[i].id + '"]').css({
+				top: views[i].verticalAlign.orientation == 'top' ? views[i].verticalAlign.distance : 'auto',
+				right: views[i].horizontalAlign.orientation == 'right' ? views[i].horizontalAlign.distance : 'auto',
+				bottom: views[i].verticalAlign.orientation == 'top' ? 'auto' : views[i].verticalAlign.distance,
+				left: views[i].horizontalAlign.orientation == 'right' ? 'auto' : views[i].horizontalAlign.distance,
+				width: views[i].width,
+				height: views[i].height,
+				'background-color': views[i].color === undefined ? (views[i].color = getRandomColor()) : views[i].color
+			});
+			
+		}
+}
+
+function getRandomColor() {
+	return '#'+(Math.random()*0xFFFFFF<<0).toString(16);	
 }
