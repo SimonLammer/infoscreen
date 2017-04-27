6c8a9c8a6e22af7c466b1943f72ce2793fa3bf70
Jakob Wögerbauer <jakob.woegerbauer@gmx.at>
2017-04-27 13:49:09 +0200

    setup on mainComponent changed event

--- src/index.js
+++ src/index.js
@@ -12,18 +12,40 @@ var editor = {
 
 Vue.component('navbar', {
     template: `
-    <h1>navbar!!</h1>test
-    `
+    <h1>navbar!!</h1>
+    `,
+    mounted() {
+        this.$bus.$on('contentChanged', event => {
+            alert(event.newContent);
+        });
+    }
 });
 Vue.component('main_content', {});
+
+// define global event bus
+Object.defineProperty(Vue.prototype, '$bus', {
+    get() {
+        return this.$root.bus;
+    }
+});
+// create event bus
+var bus = new Vue({})
 
 var app = new Vue({
   el: '#app',
   data: {
+      bus: bus, // set event bus
       currentView: 'editor'
   },
   components:{
       home: home,
       editor: editor
+  },
+  watch:{
+      currentView: function(newValue){
+          this.$bus.$emit('contentChanged', {
+            newContent: newValue
+          });
+      }
   }
 });
