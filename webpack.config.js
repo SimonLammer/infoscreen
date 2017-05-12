996aae02eddc747cacbbe412e77eff0708b34c11
Simon Lammer <lammer.simon@gmail.com>
2017-05-12 14:13:29 +0200

    Use css source map

--- webpack.config.js
+++ webpack.config.js
@@ -25,8 +25,16 @@ module.exports = {
       {
         test: /\.scss$/,
         use: ExtractTextPlugin.extract({
-          fallback:"style-loader",
-          use:["css-loader","sass-loader"]
+          fallback: "style-loader",
+          use: [{
+                loader: "css-loader", options: {
+                    sourceMap: true
+                }
+            }, {
+                loader: "sass-loader", options: {
+                    sourceMap: true
+                }
+            }]
         })
       }
     ]
