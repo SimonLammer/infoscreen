f431aa99e9d181238685e8e21da45666908eaea4
Simon Lammer <lammer.simon@gmail.com>
2017-05-22 13:11:36 +0200

    sourcemap working, webpack broken

--- karma.conf.js
+++ karma.conf.js
@@ -1,104 +1,46 @@
-// Karma configuration
-// Generated on Thu May 11 2017 10:03:47 GMT+0200 (Mitteleuropäische Sommerzeit)
-
 module.exports = function(config) {
   config.set({
-
-    // base path that will be used to resolve all patterns (eg. files, exclude)
     basePath: '',
-
-
-    // frameworks to use
-    // available frameworks: https://npmjs.org/browse/keyword/karma-adapter
-    frameworks: ['jasmine'],
-
-
-    // list of files / patterns to load in the browser
-    files: [
-      // 'https://unpkg.com/vue@2.3.0',
-      //'src/entities/*.js',
-      //'src/module_types/*.js',
-      //'src/globals.js',
-      // 'src/vue-components.js',
-      // 'src/index.js',
-      './tests/**/*.ts'
+    frameworks: [
+      'jasmine',
     ],
-
-
-    // list of files to exclude
+    files: ['tests/test_index.js'],
     exclude: [
-      
     ],
-
-
-    // preprocess matching files before serving them to the browser
-    // available preprocessors: https://npmjs.org/browse/keyword/karma-preprocessor
     preprocessors: {
-      '**/*.ts': ['typescript']
+      'tests/test_index.js': ['webpack', 'sourcemap']
     },
-/*
-    typescriptPreprocessor: {
-      options: {
-        sourceMap: true, // generate source maps
-        noResolve: false // enforce type resolution
+    webpack: {
+      module: {
+        loaders: [
+          {
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loader: 'ts-loader'
+          }
+        ]
       },
-      transformPath: function(path) {
-        return path.replace(/\.ts$/, '.js');
-      }
-    },*/
-
-    // test results reporter to use
-    // possible values: 'dots', 'progress'
-    // available reporters: https://npmjs.org/browse/keyword/karma-reporter
+      resolve: {
+        extensions: ['.js', '.ts']
+      },
+      devtool: 'inline-source-map'
+    },
+    webpackMiddleware: {
+      stats: 'errors-only'
+    },
     reporters: ['progress'],
-
-
-    // web server port
     port: 9876,
-
-
-    // enable / disable colors in the output (reporters and logs)
     colors: true,
-
-
-    // level of logging
-    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
     logLevel: config.LOG_INFO,
-
-
-    // enable / disable watching file and executing tests whenever any file changes
-    autoWatch: false,
-
-
-    // start these browsers
-    // available browser launchers: https://npmjs.org/browse/keyword/karma-launcher
+    autoWatch: true,
     browsers: [
-      /* */
       'PhantomJS',
-      /* * /
       'Chrome',
       'Firefox',
       // 'Opera',
       'IE',
-      /* */
     ],
-
-    plugins : [
-        'karma-phantomjs-launcher',
-        'karma-chrome-launcher',
-        'karma-firefox-launcher',
-        //'karma-opera-launcher',
-        'karma-ie-launcher',
-        'karma-jasmine',
-        'karma-typescript-preprocessor',
-    ],
-
-    // Continuous Integration mode
-    // if true, Karma captures browsers, runs the tests and exits
-    singleRun: true,
-
-    // Concurrency level
-    // how many browser should be started simultaneous
+    singleRun: false,
     concurrency: Infinity
-  })
-}
+  });
+};
--- tests/test.ts
+++ tests/test.ts
@@ -1,5 +1,7 @@
-describe('test', function() {
-	it('tests stuff', function() {
+import { Variable } from '../src/scripts/entities/Variable';
+
+describe('test', () => {
+	it('tests stuff', () => {
 		console.log('testing');
 		expect(2 + 2).toEqual(3);
 	});
